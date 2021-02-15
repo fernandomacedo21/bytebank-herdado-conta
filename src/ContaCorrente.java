5cecfbbbb023da729086e5d5ea352775e88b5bf9
Fernando Macedo <fernando.macedo.santana@gmail.com>
2021-02-15 17:31:04 -0300

    Praticando Heranças com Interfaces

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,7 +1,7 @@
 
 
 //new ContaCorrente
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 
 	
 	public ContaCorrente(int agencia, int numero) {
@@ -17,5 +17,10 @@ public class ContaCorrente extends Conta {
 	public boolean saca(double valor) {
 		double valorASacar = valor + 0.2;
 		return super.saca(valorASacar);
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo * 0.01;
 	}
 }
